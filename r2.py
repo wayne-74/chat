ca55e83c403d22d7eda48f13fc7fc79090d7cc07
Wayne Yang <f5v256@yahoo.com.tw>
2020-10-26 23:26:52 +0800

    add r2.py for line chat history

---
+++ r2.py
@@ -0,0 +1,58 @@
+
+def read_file(filename):
+    lines = []
+    with open(filename, 'r', encoding='utf-8-sig') as f :
+        for line in f:
+            lines.append(line.strip())
+    return lines
+
+
+def convert(lines):
+    person = None
+    allen_word_count = 0
+    allen_sticker_count = 0
+    allen_image_count = 0 
+    viki_word_count = 0
+    viki_sticker_count = 0
+    viki_image_count = 0
+    for line in lines:
+        s = line.split(' ')
+        time = s[0]
+        name = s[1]
+        if name == 'Allen':
+            if s[2] == '貼圖':
+                allen_sticker_count += 1
+            elif s[2] =='圖片':
+                allen_image_count += 1
+            else:
+                for m in s[2:]:
+                    allen_word_count += len(m)
+        if name == 'Viki':
+            if s[2] == '貼圖':
+                viki_sticker_count += 1
+            elif s[2] =='圖片':
+                viki_image_count += 1
+            else:
+                for m in s[2:]:
+                    viki_word_count += len(m)
+    print('Allen說了',allen_word_count, '個字')
+    print('Viki說了',viki_word_count, '個字')
+    print('Allen傳了',allen_sticker_count, '貼圖')
+    print('Viki傳了',viki_sticker_count, '貼圖')
+    print('Allen傳了',allen_image_count, '圖片')
+    print('Viki傳了',viki_image_count, '圖片')
+
+
+def write_file(filename, lines):
+    with open(filename, 'w') as f:
+        for line in lines:
+            f.write(line + '\n')  
+
+
+def main():
+    lines = read_file('LINE-Viki.txt')
+    lines = convert(lines)
+    # write_file('output.txt', lines)
+
+
+main()
